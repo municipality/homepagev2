22a4581260567ec984e552c8de7c845f3ee10f0d
Brian <amunicipality@gmail.com>
2016-07-05 17:25:48 -0700

    bar changes (kind of)

--- app/bar.component.ts
+++ app/bar.component.ts
@@ -1,14 +1,14 @@
-import {Component} from 'angular2/core';
+import {Component, ViewChild, AfterViewInit} from 'angular2/core';
 import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
 @Component ({
     selector : 'bar',
     directives: [ROUTER_DIRECTIVES],
     template : `
-        <div class="container">
+        <div class="container" #container>
             <div [ngClass]="{'logo': i==0, 'icon': i!=0,
                              'active': router.isRouteActive(router.generate([item.name])) }"
             *ngFor="#item of items; #i = index"
-            [routerLink]="[item.name]">
+            [routerLink]="[item.name]" (click)="click(item.name)">
                 <img src={{item.icon}}>
                 <p *ngIf="i != 0">{{item.display}}</p>
             </div>
@@ -16,8 +16,9 @@ import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
     `
 })
 
-export class Bar {
+export class Bar implements AfterViewInit {
     items : Object[];
+    @ViewChild('container') container;
     constructor (private router : Router) {
         this.items = [
             {
@@ -36,7 +37,7 @@ export class Bar {
                 name : "Blog",
                 display : "B Log",
                 path : "/blog",
-                icon : ""
+                icon : "icons/blog-icon.png"
             },
             {
                 name : "Projects",
@@ -46,5 +47,23 @@ export class Bar {
             },
 
         ];
+    }
+
+    ngAfterViewInit () {
+        // var router = this.router;
+        // if(router.isRouteActive(router.generate(["Adventures"]))) {
+        //     this.container.nativeElement.style["background-color"] = "blue";
+        // }
+    }
+
+    click (name) {
+        // var router = this.router;
+        // //if(router.isRouteActive(router.generate(["Adventures"]))) {
+        // if(name == "Adventures") {
+        //     this.container.nativeElement.style["background-color"] = "blue";
+        // }
+        // else {
+        //     this.container.nativeElement.style["background-color"] = "";
+        // }
     }
 }
